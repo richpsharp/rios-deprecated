1c158054ca7037ffd10359b92a4b3a246d94fffa
Rich Sharp <richpsharp@gmail.com>
2015-08-11 16:38:36 -0700

    re #11 adding multiprocessing and pygeoprocessing hidden imports

--- exescripts/hooks/hook-natcap.rios.py
+++ exescripts/hooks/hook-natcap.rios.py
@@ -1,3 +1,4 @@
 from PyInstaller.hooks.hookutils import collect_data_files, collect_submodules
 datas = collect_data_files('natcap.rios') + collect_data_files('natcap.rios.rui')
-hiddenimports = collect_submodules('natcap.rios') + collect_submodules('natcap.rios.rui')
+hiddenimports = collect_submodules('natcap.rios') + collect_submodules('natcap.rios.rui') + collect_submodules('multiprocessing')
+
